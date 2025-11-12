7396e810c7ce5dbd72c54daefd59e904d4227189
PedroTsI <cantalice.pedro@academico.ifpb.edu.br>
2025-11-12 17:02:46 -0300

    SQLite insertion, Style Changes

---
+++ src/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { Suspense } from 'react'
+import { Stack } from 'expo-router'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+
+import {SQLiteProvider} from "expo-sqlite"
+
+import { migrate } from '@/database/migrate'
+
+import { colors } from '@/theme/colors'
+
+import { Loading } from '@/components/Loading'
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <Suspense fallback={<Loading/>}>
+      <SQLiteProvider databaseName='target.db' onInit={migrate} useSuspense>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.white },
+        }}
+      />
+      </SQLiteProvider>
+    </Suspense>
+  )
+}
---
+++ src/app/target.tsx
@@ -0,0 +1,65 @@
+import { use, useState } from 'react'
+import { Button } from '@/components/Button'
+import { CurrencyInput } from '@/components/CurrencyInput'
+import { Input } from '@/components/Input'
+import { PageHeader } from '@/components/PageHeader'
+import { Alert, View } from 'react-native'
+import { useLocalSearchParams, router } from 'expo-router'
+
+export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [name, setName] = useState("")
+  const [amount, setAmount] = useState(0)
+  const params = useLocalSearchParams<{id?: string}>()
+
+  function handleSave(){
+    if(!name.trim() || amount <= 0){
+      return Alert.alert("Atencao", "Preencha nome e valor.")
+    }
+
+    setIsProcessing(true)
+
+    if(params.id){
+      //update
+    } else {
+      create()
+    }
+
+  }
+
+  async function create(){
+    try {
+      Alert.alert("Nova Meta", "Meta criada com sucesso!", [
+        {
+          text: "Ok",
+          onPress: () => router.back()
+        },
+      ])
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel criar a meta.")
+      console.log(error)
+      setIsProcessing(false)
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
+      />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nova meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+          onChangeText={setName} value={name}
+        />
+
+        <CurrencyInput label="Valor alvo (R$)" value={amount} onChangeValue={setAmount} />
+
+        <Button title="Salvar" onPress={handleSave} isProcessing={isProcessing}/>
+      </View>
+    </View>
+  )
+}
