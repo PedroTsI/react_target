58fee29012fe4ba2108005be3573176821cc6817
PedroTsI <cantalice.pedro@academico.ifpb.edu.br>
2025-11-19 17:57:31 -0300

    Targets Database Visualization

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
+import { useCallback, useState} from 'react'
+import { Alert, StatusBar, View } from 'react-native'
+import { router, useFocusEffect } from 'expo-router'
+
 import { Button } from '@/components/Button'
 import { HomeHeader } from '@/components/HomeHeader'
 import { List } from '@/components/List'
-import { Target } from '@/components/Target'
-import { router } from 'expo-router'
-import { StatusBar, View } from 'react-native'
+import { Target, TargetProps} from '@/components/Target'
+
+import { useTargetDatabase } from '@/database/useTargetDatabase'
+import { Loading } from '@/components/Loading'
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -11,31 +16,48 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883,65' },
 }
 
-const targets = [
-  {
-    id: '1',
-    name: 'Apple Watch',
-    percentage: '50%',
-    current: 'R$ 580,00',
-    target: 'R$ 1.790,00',
-  },
-  {
-    id: '2',
-    name: 'Comprar uma cadeira ergonômica',
-    percentage: '75%',
-    current: 'R$ 900,00',
-    target: 'R$ 1.200,00',
-  },
-  {
-    id: '3',
-    name: 'Comprar uma cadeira ergonômica',
-    percentage: '75%',
-    current: 'R$ 1.200,00',
-    target: 'R$ 3.000,00',
-  },
-]
+
 
 export default function Index() {
+  const [isFetching, setIsFetching] = useState(true)
+  const [targets, setTargets] = useState<TargetProps[]>([])
+
+  const targetDatabase = useTargetDatabase()
+
+  async function fetchTargets(): Promise<TargetProps[]> {
+    try {
+      const response = await targetDatabase.listBySavedValue()
+      
+      return response.map((item) => ({
+        id: String(item.id),
+        name: item.name,
+        current: String(item.current),
+        percentage: item.percentage.toFixed(0) + "%",
+        target: String(item.amount),
+      }))
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel carregar as metas.")
+      console.log(error)
+    }
+  }
+
+  async function fetchData(){
+    const targetDataPromise = fetchTargets()
+
+    const [targetData] = await Promise.all([targetDataPromise])
+
+    setTargets(targetData)
+    setIsFetching(false)
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchData()
+  }, []))
+
+  if (isFetching){
+    return <Loading/>
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
