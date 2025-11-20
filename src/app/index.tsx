a5ad016c0b399813353b3cd62e07a6a772df1eb8
PedroTsI <cantalice.pedro@academico.ifpb.edu.br>
2025-11-20 16:57:07 -0300

    database target integration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,15 @@ import { useCallback, useState} from 'react'
 import { Alert, StatusBar, View } from 'react-native'
 import { router, useFocusEffect } from 'expo-router'
 
-import { Button } from '@/components/Button'
-import { HomeHeader } from '@/components/HomeHeader'
 import { List } from '@/components/List'
+import { Button } from '@/components/Button'
+import { Loading } from '@/components/Loading'
+import { HomeHeader } from '@/components/HomeHeader'
 import { Target, TargetProps} from '@/components/Target'
 
 import { useTargetDatabase } from '@/database/useTargetDatabase'
-import { Loading } from '@/components/Loading'
+
+import { numberToCurrency } from '@/utils/numberToCurrency'
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -31,9 +33,9 @@ export default function Index() {
       return response.map((item) => ({
         id: String(item.id),
         name: item.name,
-        current: String(item.current),
+        current: numberToCurrency(item.current),
         percentage: item.percentage.toFixed(0) + "%",
-        target: String(item.amount),
+        target: numberToCurrency(item.amount),
       }))
     } catch (error) {
       Alert.alert("Erro", "Não foi possivel carregar as metas.")
